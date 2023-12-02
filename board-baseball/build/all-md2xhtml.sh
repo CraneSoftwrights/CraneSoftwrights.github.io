171ef28be04b2f18fd614d664a1d89e1c85f6db4
gkholman <gkenholman+github@gmail.com>
2023-12-01 22:16:03 -0500

    Update baseball documentation

--- board-baseball/build/all-md2xhtml.sh
+++
@@ -1,4 +0,0 @@
-find . -name \*.md -exec sh -c 'sh build/md2xhtml.sh <{} >{}.html' \;
-if [ -f md2xhtml.zip ]; then rm md2xhtml.zip ; fi
-find . -type f -name "*.md.html" -exec zip md2xhtml.zip {} \;
-zip -r md2xhtml.zip shared/* build/*
